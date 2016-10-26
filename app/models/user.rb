1355322f0876493e7282436897f8c55dcf7e0232
Cornelius Soon <cornelius.soon@gmail.com>
2016-10-26 20:44:14 +1300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   
   has_secure_password
   validates :password, presence: true,
-                       length: { minimum: 6 }
+                       length: { minimum: 5 }
   
 end
 
